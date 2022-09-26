aaf121c4f019f6d0b74eaf242a04ef23fbae7747
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-09-26 07:19:52 -0300

    feat: Prisma ORM and models

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) => {
-    return res.json([
-        {
-            id: 1,
-            name: "Anúncio 1",
-        },
-        {
-            id: 2,
-            name: "Anúncio 2",
-        },
-        {
-            id: 3,
-            name: "Anúncio 3",
-        },
-    ]);
+app.get("/games", (req, res) => {
+    return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+    return res.status(201).json([]);
+});
+
+app.get(`/games/:id/ads`, (req, res) => {
+    return res.json([]);
+});
+
+app.get(`/ads/:id/discord`, (req, res) => {
+    return res.json([]);
 });
 
 app.listen(3333);
